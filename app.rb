19023f26cdcb05420528e7002ee746e79fb0e596
Your Name <you@example.com>
2019-01-30 15:04:52 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,34 @@
 require_relative 'config/environment'
-
+# require 'pry'
 class App < Sinatra::Base
 
-end
+  get "/" do
+    # @puppies = Puppy.all
+    erb :index
+  end
+
+  get "/new" do
+    erb :create_puppy
+  end
+
+  post "/puppy" do
+
+    @puppy = Puppy.new(params["name"], params["breed"], params["age"])
+    erb :display_puppy
+  end
+
+  # get '/' do
+  #   erb :index
+  # end
+  #
+  # get '/new' do
+  #   erb :create_puppy
+  # end
+  #
+  # post '/puppy' do
+  #   # @puppy = Puppy.new(params['name'], params['breed'], params['age'])
+  #
+  #   @puppy = Puppy.new(params)
+  #   erb :display_puppy
+  #   # redirect to ("")
+  end
